307413043e33540cbe9c3746ef420261f8138315
Jörn Jacobsen <mail@henrik-jacobsen.com>
2021-02-16 12:55:41 +0100

    Update iRevNet.py

--- models/iRevNet.py
+++ models/iRevNet.py
@@ -152,7 +152,7 @@ class iRevNet(nn.Module):
 
 if __name__ == '__main__':
     model = iRevNet(nBlocks=[6, 16, 72, 6], nStrides=[2, 2, 2, 2],
-                    nChannels=None, nClasses=1000, init_ds=2,
+                    nChannels=[24, 96, 384, 1536], nClasses=1000, init_ds=2,
                     dropout_rate=0., affineBN=True, in_shape=[3, 224, 224],
                     mult=4)
     y = model(Variable(torch.randn(1, 3, 224, 224)))
